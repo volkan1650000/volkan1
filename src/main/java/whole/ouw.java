412213a9a522895203c8756ad04d9f25ed135f38
Ertugrul Onal <oenalertugrul@gmail.com>
2023-05-15 14:34:24 +0200

    second Commit

--- src/main/java/whole/ouw.java
+++ src/main/java/whole/ouw.java
@@ -26,6 +26,7 @@ public class ouw {
                 }
                 list.add(s.charAt(counter));
                 max=Math.max(max,count);
+                System.out.println(" deneme yazisi bakalim eklenecek mi");
             }
         }
         return max;
